67b12abd42493238e3db7823d4fd8e578549a65f
Jefferson Schmitt <jeffersonschmitt@live.com>
2026-07-21 11:23:32 -0300

    Init domain

---
+++ BookingService/Core/Domain/Entities/Room.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int level { get; set; }
+        public bool InMaintenance { get; set; }
+
+        public bool IsAvailable
+        {
+            get
+            {
+                if (this.InMaintenance || this.hasGuest)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+        public bool hasGuest
+        {
+            //Verificar se existem reservas ativas para este quarto
+            get { return true; }
+        }
+    }
+}
