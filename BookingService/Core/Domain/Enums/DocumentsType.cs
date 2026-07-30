ede7713d0a508c84cd34f6a4920ce0dcf3532799
Jefferson Schmitt <jeffersonschmitt@live.com>
2026-07-30 16:22:21 -0300

    ValueObjects

---
+++ BookingService/Core/Domain/Enums/DocumentsType.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Enums
+{
+    public enum DocumentsType
+    {
+        Passport = 0,
+        DriverLicense = 1,
+        IdentityCard = 2
+    }
+}
---
+++ BookingService/Core/Domain/ValueObjects/PersonId.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Enums;
+
+namespace Domain.ValueObjects
+{
+    public class PersonId
+    {
+        public string IdNumber { get; set; }
+        public DocumentsType DocumentType { get; set; }
+    }
+}
