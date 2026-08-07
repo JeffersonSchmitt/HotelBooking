9b5ee1e80cb0653bffbcbccece8b4ec9cbe6d60d
Jefferson Schmitt <jeffersonschmitt@live.com>
2026-08-07 15:15:39 -0300

    Lógica de négocio e exepctions ao salvar um Guest

--- BookingService/Core/Domain/Entities/Guest.cs
+++ BookingService/Core/Domain/Entities/Guest.cs
@@ -1,4 +1,7 @@
-﻿using Domain.ValueObjects;
+﻿using Domain.Exceptions;
+using Domain.ValueObjects;
+using Domain.Utils;
+using Domain.Ports;
 
 namespace Domain.Entities
 {
@@ -9,5 +12,33 @@ namespace Domain.Entities
         public string Surname { get; set; }
         public string Email { get; set; }
         public PersonId Document { get; set; }
+
+        private void ValidateState()
+        {
+            if (Document.IdNumber == null || Document.IdNumber.Length <= 3 || Document.DocumentType <= 0)
+            {
+                throw new InvalidPersonDocumentIdException();
+            }
+            if (Name == null || Surname == null || Email == null)
+            {
+                throw new MissingRequiredInformationException();
+            }
+            if (UtilsEmail.ValidateEmail(this.Email) == false)
+            {
+                throw new InvalidEmailException();
+            }
+        }
+        public async Task Save(IGuestRepository guestRepository)
+        {
+            ValidateState();
+            if (this.Id == 0)
+            {
+                this.Id = await guestRepository.Create(this);
+            }
+            else
+            {
+                // await guestRepository.Update(this);
+            }
+        }
     }
 }
